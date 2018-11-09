feb6f70737e46e7dcc0a6ce873a53176063fa69f
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-11-09 14:52:15 +0100

    Initial commit

---
+++ js/1_create_array.js
@@ -0,0 +1,30 @@
+/* INSTRUCTIONS
+To run this file, click in the Console below and type: node 1_create_array.js 
+If the console isn't visible, click the View menu above and choose Show Console.
+
+You can clear the console by typing `clear` and pressing enter.
+
+If your program is stuck in an infinite loop, you can break out of the program by typing ctrl + C.
+*/
+
+// 1. Create a new array named languages that contains at least 7 programming languages.
+
+// 2. Use console.log to display the number of elements in the array:
+
+/* 3. Use console.log to list the first element from the array using an index value. */
+
+/* 4. Use console.log to list the last element from the array using an index value. */
+
+// 5. Use an array method to add an element to the END of the array.
+
+// 6. Use an array method to add an element to the BEGINNING of the array.
+
+/* 7. Log all of the elements in the array as a single string using the array .join() method */
+
+// 8. Use an array method to REMOVE the LAST element from the array.
+
+// 9. Use an array method to REMOVE the first element from the array.
+
+/* 10. Log all of the elements in the array as a single string using the array .join() method. Thisis the same as step 7 above */
+
+// Run your code by typing node 1_create_array.js in the console below
